ccc73be033ab3e8d63cb50a2a93baf39c27b56cf
allyesok <gamehi@163.com>
2026-01-12 17:01:07 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3768,6 +3768,6 @@ io.on("connection", (socket) => {
   });
 });
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
